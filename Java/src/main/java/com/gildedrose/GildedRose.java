4dd4b1855d1d87ddd675633d801935c069bb5f1a
vineeta <vineeta.khurana27@gmail.com>
2024-11-14 16:51:09 -0500

    refactor updatequality and add GildedRoseTest

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,23 +14,24 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        boolean update = false;
+        boolean canUpdateItemXQuality = false;
         if(days == 0) {
-            update = true;
+            canUpdateItemXQuality = true;
             days = 1;
         }else {
             days--;
         }
         for (Item item: items) {
-            if (!item.name.contains(AGED_BRIE)
-                    && !item.name.contains(BACKSTAGE_PASSES)) {
+            String name = item.name;
+            if (!isAgedBrie(name)
+                    && !isBackstagePasses(name)) {
                 if (item.quality > 0) {
-                    if(item.name.contains(CONJURED)) {
+                    if(isConjured(name)) {
                         item.quality = item.quality - 1;
                     }
-                    if (!item.name.contains(SULFURAS) && !isX(item.name)){
+                    if (!isSulfuras(name) && !isX(name)){
                         item.quality = item.quality - 1;
-                    }else if(isX(item.name) && update) {
+                    }else if(isX(name) && canUpdateItemXQuality) {
                         item.quality = item.quality - 1;
                     }
                 }
@@ -38,7 +39,7 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.contains(BACKSTAGE_PASSES)) {
+                    if (isBackstagePasses(name)) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -54,15 +55,15 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.contains(SULFURAS)) {
+            if (!isSulfuras(name)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.contains(AGED_BRIE)) {
-                    if (!item.name.contains(BACKSTAGE_PASSES)) {
+                if (!isAgedBrie(name)) {
+                    if (!isBackstagePasses(name)) {
                         if (item.quality > 0) {
-                            if (!item.name.contains(SULFURAS)) {
+                            if (!isSulfuras(name)) {
                                 item.quality = item.quality - 1;
                             }
                         }
@@ -80,5 +81,21 @@ class GildedRose {
 
     private boolean isX(String name) {
         return (name.contains("X") || name.contains("x"));
+    }
+
+    private boolean isAgedBrie(String name) {
+        return name.contains(AGED_BRIE);
+    }
+
+    private boolean isSulfuras(String name) {
+        return name.contains(SULFURAS);
+    }
+
+    private boolean isBackstagePasses(String name) {
+        return name.contains(BACKSTAGE_PASSES);
+    }
+
+    private boolean isConjured(String name) {
+        return name.contains(CONJURED);
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,80 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void sulfurasTest() {
+        Item[] items = new Item[] { new Item("Sulfuras", 5, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras", app.items[0].name);
+        assertEquals(5, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    void conjuredTest() {
+        Item[] items = new Item[] { new Item("Conjured item 1", 10, 8) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured item 1", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(6, app.items[0].quality);
+    }
+
+    @Test
+    void agedBrieTest() {
+        Item[] items = new Item[] { new Item("Aged Brie cheese", 6, 45) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie cheese", app.items[0].name);
+        assertEquals(5, app.items[0].sellIn);
+        assertEquals(46, app.items[0].quality);
+    }
+
+    @Test
+    void xItem() {
+        Item[] items = new Item[] { new Item("Exxon gift card", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Exxon gift card", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(10, app.items[0].quality);
+
+        app.updateQuality();
+        assertEquals("Exxon gift card", app.items[0].name);
+        assertEquals(8, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesTest() {
+        Item[] items = new Item[] { new Item("Backstage passes to Band", 10, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to Band", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(7, app.items[0].quality);
+    }
+
+    @Test
+    void itemTest() {
+        Item[] items = new Item[] { new Item("Eggs", 10, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Eggs", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
+    }
+
+    @Test
+    void itemQuality50Test() {
+        Item[] items = new Item[] { new Item("Backstage passes", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+
 }
