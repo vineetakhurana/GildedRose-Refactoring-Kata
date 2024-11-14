fc4ac226e4dee1c9cbd37a17a7a1d23aa3fca8c5
vineeta <vineeta.khurana27@gmail.com>
2024-11-14 16:15:54 -0500

    add constants and change equals to contains for item name match

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,29 +2,33 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
-    int days = 1;
-    boolean update = false;
+    private int days = 1;
+
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes";
+    private static final String CONJURED = "Conjured";
+    private static final String SULFURAS = "Sulfuras";
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
+        boolean update = false;
         if(days == 0) {
             update = true;
             days = 1;
         }else {
-            update = false;
             days--;
         }
         for (Item item: items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!item.name.contains(AGED_BRIE)
+                    && !item.name.contains(BACKSTAGE_PASSES)) {
                 if (item.quality > 0) {
-                    if(item.name.contains("Conjured")) {
+                    if(item.name.contains(CONJURED)) {
                         item.quality = item.quality - 1;
                     }
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros") && !isX(item.name)){
+                    if (!item.name.contains(SULFURAS) && !isX(item.name)){
                         item.quality = item.quality - 1;
                     }else if(isX(item.name) && update) {
                         item.quality = item.quality - 1;
@@ -34,7 +38,7 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.name.contains(BACKSTAGE_PASSES)) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -50,15 +54,15 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!item.name.contains(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!item.name.contains(AGED_BRIE)) {
+                    if (!item.name.contains(BACKSTAGE_PASSES)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!item.name.contains(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
