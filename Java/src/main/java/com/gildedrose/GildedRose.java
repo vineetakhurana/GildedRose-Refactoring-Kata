f02372221a2cf79c4d172dda7d8929f63a1b1c31
vineeta <vineeta.khurana27@gmail.com>
2024-11-15 13:23:43 -0500

    refactor and format and split logic into methods

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,13 +1,12 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
-    private int days = 1;
-
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes";
     private static final String CONJURED = "Conjured";
     private static final String SULFURAS = "Sulfuras";
+    Item[] items;
+    private int days = 1;
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -15,66 +14,32 @@ class GildedRose {
 
     public void updateQuality() {
         boolean canUpdateItemXQuality = false;
-        if(days == 0) {
+        if (days == 0) {
             canUpdateItemXQuality = true;
             days = 1;
-        }else {
+        } else {
             days--;
         }
-        for (Item item: items) {
-            String name = item.name;
-            if (!isAgedBrie(name)
-                    && !isBackstagePasses(name)) {
-                if (item.quality > 0) {
-                    if(isConjured(name)) {
-                        item.quality = item.quality - 1;
-                    }
-                    if (!isSulfuras(name) && !isX(name)){
-                        item.quality = item.quality - 1;
-                    }else if(isX(name) && canUpdateItemXQuality) {
-                        item.quality = item.quality - 1;
-                    }
+        for (Item item : items) {
+
+            //Sulfuras not sold, quality does not change, no change needed
+            if (isSulfuras(item.name)) {
+                continue;
+            }
+
+            if (!isAgedBrie(item.name) && !isBackstagePasses(item.name)) {
+                if (isItemQualityPositive(item)) {
+                    reduceQuality(item, canUpdateItemXQuality);
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (isBackstagePasses(name)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+                increaseQuality(item);
             }
 
-            if (!isSulfuras(name)) {
-                item.sellIn = item.sellIn - 1;
-            }
+            //sell in reduces by 1 for all but Sulfuras
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
-                if (!isAgedBrie(name)) {
-                    if (!isBackstagePasses(name)) {
-                        if (item.quality > 0) {
-                            if (!isSulfuras(name)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+                updateQualityForItemsPastSellIn(item);
             }
         }
     }
@@ -97,5 +62,62 @@ class GildedRose {
 
     private boolean isConjured(String name) {
         return name.contains(CONJURED);
+    }
+
+    private void reduceQuality(Item item, boolean canUpdateItemXQuality) {
+        if (isConjured(item.name)) {
+            //conjured item quality will reduce by 2
+            item.quality = item.quality - 1;
+        }
+        if (!isX(item.name) || (isX(item.name) && canUpdateItemXQuality)) {
+            //reduce all eligible items quality daily or item "x/X" by 1 every 2 days
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        if (isItemQualityBelowMax(item)) {
+            //Aged brie + 1
+            //Backstage passes can go upto +3 based on demand
+            item.quality = item.quality + 1;
+
+            if (isBackstagePasses(item.name)) {
+                if (item.sellIn < 11) {
+                    if (isItemQualityBelowMax(item)) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (isItemQualityBelowMax(item)) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private void updateQualityForItemsPastSellIn(Item item) {
+        if (!isAgedBrie(item.name)) {
+            if (!isBackstagePasses(item.name)) {
+                if (isItemQualityPositive(item)) {
+                    item.quality = item.quality - 1;
+                }
+            } else {
+                item.quality = 0;
+            }
+        } else {
+            if (isItemQualityBelowMax(item)) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private boolean isItemQualityPositive(Item item) {
+        return item.quality > 0;
+    }
+
+    private boolean isItemQualityBelowMax(Item item) {
+        return item.quality < 50;
     }
 }
